06774daf472d2dc9f42d46621be52cc20f0bee64
harizaldycahya <zaldypratama82@gmail.com>
2025-12-22 15:06:54 +0700

    testing shadcn ui

--- uar/app/layout.tsx
+++ uar/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          <h1>TRIASMITRA</h1>
+        </nav>
         {children}
       </body>
     </html>
